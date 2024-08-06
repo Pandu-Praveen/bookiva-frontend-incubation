a3a809a0d12706cba0ee0c3b0f0f872b453f462d
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2024-08-06 17:13:48 +0530

    Update contactus.js

--- contactus.js
+++ contactus.js
@@ -1,1 +1,32 @@
-
+import { API_LOCAL } from "./config";
+feather.replace();
+localStorage.setItem("x", 1);
+fetch(API_LOCAL + "/profile", {
+// Include cookies with the request
+headers: {
+  'Authorization': `Bearer ${token}`,
+  "Content-Type": "application/json",
+  },
+  credentials: "include",
+})
+.then((res) => {
+  return res.json();
+})
+.then((data) => {
+    console.log(data);
+    const atag=document.getElementById("reservebtn");
+    if (data.blockStatus) {
+      atag.setAttribute('href', '/block/');
+      // location.href = "/block/";
+      console.log("blockedddddd");
+    } else if (data.role == "MANAGEMENT") {
+      atag.setAttribute('href', '/management/');
+      // location.href = "/management/";
+    } else if (data.role == "ADMIN") {
+      atag.setAttribute('href', '/admin/');
+      // location.href = "/admin/";
+    } else if (data.role == "USER") {
+      atag.setAttribute('href', '/venues/');
+      // location.href = "/venues/";
+    }
+ });
