9e1fea5ef3c6c73a29370843b3879d540704eaca
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2024-08-06 17:47:18 +0530

    Update contactus.js

--- contactus.js
+++ contactus.js
@@ -1,36 +1,34 @@
 import { API_LOCAL } from "./config";
 import Cookies from "js-cookie";
+import feather from 'feather-icons';
+
 document.addEventListener("DOMContentLoaded", function() {
-const token =  Cookies.get("jwt");
-feather.replace();
-localStorage.setItem("x", 1);
-fetch(API_LOCAL + "/profile", {
-// Include cookies with the request
-headers: {
-  'Authorization': `Bearer ${token}`,
-  "Content-Type": "application/json",
-  },
-  credentials: "include",
-})
-.then((res) => {
-  return res.json();
-})
-.then((data) => {
-    console.log(data);
-    const atag=document.getElementById("reservebtn");
-    if (data.blockStatus) {
-      atag.setAttribute('href', '/block/');
-      // location.href = "/block/";
-      console.log("blockedddddd");
-    } else if (data.role == "MANAGEMENT") {
-      atag.setAttribute('href', '/management/');
-      // location.href = "/management/";
-    } else if (data.role == "ADMIN") {
-      atag.setAttribute('href', '/admin/');
-      // location.href = "/admin/";
-    } else if (data.role == "USER") {
-      atag.setAttribute('href', '/venues/');
-      // location.href = "/venues/";
-    }
- });
-}
+    const token = Cookies.get("jwt");
+    feather.replace();
+    localStorage.setItem("x", 1);
+
+    fetch(API_LOCAL + "/profile", {
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        const atag = document.getElementById("reservebtn");
+        if (data.blockStatus) {
+            atag.setAttribute('href', '/block/');
+            console.log("blockedddddd");
+        } else if (data.role == "MANAGEMENT") {
+            atag.setAttribute('href', '/management/');
+        } else if (data.role == "ADMIN") {
+            atag.setAttribute('href', '/admin/');
+        } else if (data.role == "USER") {
+            atag.setAttribute('href', '/venues/');
+        }
+    });
+});
