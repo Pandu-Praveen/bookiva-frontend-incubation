be32448ce650c2153c669ccc22473c3ddd525500
PRAVEEN S <130171193+Pandu-Praveen@users.noreply.github.com>
2026-04-03 14:28:43 +0530

    Log card data and image URL in venue.js
    Added console log to display card data and image URL.

--- venue.js
+++ venue.js
@@ -133,6 +133,7 @@ function renderVenues(venues) {
   const container = document.querySelector(".cards-container");
   // Clear previous content
   venues.forEach((cardData) => {
+    console.log(cardData,cardData.imgUrl);
     renderedCards += `<div class="hall-card">
             <div class="hall-img">
                 <img src="${cardData.imgUrl}" alt="${cardData.hallName}" 
